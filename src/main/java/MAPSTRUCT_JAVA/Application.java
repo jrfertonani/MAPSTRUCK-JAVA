756722a2a00ed44f75f0629cb86f4ffdea2e2e17
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-27 18:25:09 -0300

    Application

--- src/main/java/MAPSTRUCT_JAVA/Application.java
+++ src/main/java/MAPSTRUCT_JAVA/Application.java
@@ -1,7 +1,9 @@
 package MAPSTRUCT_JAVA;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +12,10 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(){
+		return args -> {
+            // Use the generated mapper here
+        };
+	}
 }
--- src/main/java/MAPSTRUCT_JAVA/dto/UsuearioRequest.java
+++ src/main/java/MAPSTRUCT_JAVA/dto/UsuearioRequest.java
@@ -8,5 +8,5 @@ public class UsuearioRequest {
     private String nome;
     private String email;
     private String senha;
-    
+
 }
