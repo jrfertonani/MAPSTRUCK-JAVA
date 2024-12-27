7196d7c33d90cf59c3aaa1404a8c5e680cc09a7f
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-12-27 18:22:31 -0300

    Entity Usuario

---
+++ src/main/java/MAPSTRUCT_JAVA/entity/UsuarioEntity.java
@@ -0,0 +1,13 @@
+package MAPSTRUCT_JAVA.entity;
+
+import lombok.Data;
+
+@Data
+public class UsuarioEntity {
+
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+    
+}
